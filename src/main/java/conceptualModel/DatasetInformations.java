051a25234d7eb6fce4f7d5525e21575edf95e8c2
PH Paris <pierrehenri.paris@gmail.com>
2018-11-14 10:08:27 +0100

    Minor update

--- src/main/java/conceptualModel/DatasetInformations.java
+++ src/main/java/conceptualModel/DatasetInformations.java
@@ -166,18 +166,22 @@ public class DatasetInformations {
             tabular += "l|";
         }
 
-        String dbpediaPath = args.length > 1 ? args[0] : "dataset.hdt";
-        log.debug("DBpedia path: " + dbpediaPath);
-        List<String> dbpediaLatexArray = generateLatexArray(dbpediaClasses, thresholds, dbpediaPath, propertyTypeDBpedia, "DBpedia number of predicates by classes and thresholds", "tab:dbpPredicatesThresholds", columns, tabular);
-        Path dbpediaResultPath = Paths.get("dbp_results.txt");
-        Files.write(dbpediaResultPath, dbpediaLatexArray, Charset.forName("UTF-8"));
-        
-        String wikidataPath = args[1];
-        log.debug("Wikidata path: " + wikidataPath);
-        List<String> wikidataLatexArray = generateLatexArray(wikidataClasses, thresholds, wikidataPath, propertyTypeWikidata, "Wikidata number of predicates by classes and thresholds", "tab:wikiPredicatesThresholds", columns, tabular);                
-        Path wikidataResultPath = Paths.get("wikidata_results.txt");
-        Files.write(wikidataResultPath, wikidataLatexArray, Charset.forName("UTF-8"));
+        try {
+            // String dbpediaPath = args.length > 1 ? args[0] : "dataset.hdt";
+            // log.debug("DBpedia path: " + dbpediaPath);
+            // List<String> dbpediaLatexArray = generateLatexArray(dbpediaClasses, thresholds, dbpediaPath, propertyTypeDBpedia, "DBpedia number of predicates by classes and thresholds", "tab:dbpPredicatesThresholds", columns, tabular);
+            // Path dbpediaResultPath = Paths.get("dbp_results.txt");
+            // Files.write(dbpediaResultPath, dbpediaLatexArray, Charset.forName("UTF-8"));
 
+            String wikidataPath = args[1];
+            log.debug("Wikidata path: " + wikidataPath);
+            List<String> wikidataLatexArray = generateLatexArray(wikidataClasses, thresholds, wikidataPath, propertyTypeWikidata, "Wikidata number of predicates by classes and thresholds", "tab:wikiPredicatesThresholds", columns, tabular);                
+            Path wikidataResultPath = Paths.get("wikidata_results.txt");
+            Files.write(wikidataResultPath, wikidataLatexArray, Charset.forName("UTF-8"));
+        } catch (Exception e) {
+            log.error("Error in main: ", e);
+        }
+        
         log.debug("end!");
     }
 
